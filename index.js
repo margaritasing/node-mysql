97ed3772672b875dfeb4d5310486f9d3bc1beb4e
margaritasing <libiamfreitesm@gmail.com>
2022-06-12 12:14:48 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/', (req, res) => {
+    res.send("Hola Mundo")
+})
+
+app.listen(5000, ()=> {
+    console.log("Servidor arrancado")
+})
