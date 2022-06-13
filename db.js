0f494d2a2af6250c224d8292d76178cbb7e69772
margaritasing <libiamfreitesm@gmail.com>
2022-06-13 10:58:51 -0300

    agrego config de db

--- db.js
+++ db.js
@@ -1,5 +1,20 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('')
+const FilmModel = require('./models/films')
 
+const sequelize = new Sequelize('node','root','123456', {
+    host: '127.0.0.1',
+    dialect:'mysql'
+});
+
+const Film = FilmModel(sequelize, Sequelize);
+
+sequelize.sync({force: false})
+.then(()=>{
+    console.log('Tablas sincronizadas')
+})
+
+module.exports ={
+    Film
+}
 
