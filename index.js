25e2a0f9e15312c09d9158ee2ab130809de3e018
margaritasing <libiamfreitesm@gmail.com>
2022-06-16 18:41:24 -0300

    agrego config de la db

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const apiRouter = require('./routes/api')
+
 const app = express()
 
 require('./db')
@@ -8,9 +10,7 @@ require('./db')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/', (req, res) => {
-    res.send("Hola Mundo")
-})
+app.use('/api', apiRouter)
 
 app.listen(5000, ()=> {
     console.log("Servidor arrancado")
