e6419a00d7cf1e1574e6ddf275a221802f0c3e9f
margaritasing <libiamfreitesm@gmail.com>
2022-06-17 12:19:30 -0300

    agrego el user

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
-const FilmModel = require('./models/films')
+const FilmModel = require('./models/films');
+const UserModel = require('./models/user');
 
 const sequelize = new Sequelize('nsY6MVxofg','nsY6MVxofg','Ij4i5iQMax', {
     host: 'remotemysql.com',
@@ -8,13 +9,15 @@ const sequelize = new Sequelize('nsY6MVxofg','nsY6MVxofg','Ij4i5iQMax', {
 });
 
 const Film = FilmModel(sequelize, Sequelize);
+const User = UserModel(sequelize, Sequelize);
 
 sequelize.sync({force: false})
 .then(()=>{
     console.log('Tablas sincronizadas')
-})
+});
 
 module.exports ={
-    Film
-}
+    Film,
+    User
+};
 
