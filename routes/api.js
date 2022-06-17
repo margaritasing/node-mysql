87468b468198d2c364524e5dfa1831c8d19cf128
margaritasing <libiamfreitesm@gmail.com>
2022-06-17 12:20:51 -0300

    agrego rutas

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiFilmRouter = require('./api/films');
+const apiUserRouter = require('./api/users');
 
 router.use('/films', apiFilmRouter);
+router.use('/users', apiUserRouter);
 
 
 module.exports = router;
