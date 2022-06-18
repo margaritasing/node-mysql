5672f4f3acba7defbb180eb181f7bc42f27cb2fa
margaritasing <libiamfreitesm@gmail.com>
2022-06-18 16:07:14 -0300

    agrego cambios en el index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const apiRouter = require('./routes/api')
+
 
 const app = express()
 
@@ -12,6 +12,6 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.use('/api', apiRouter)
 
-app.listen(5000, ()=> {
+app.listen(3000, ()=> {
     console.log("Servidor arrancado")
 })
