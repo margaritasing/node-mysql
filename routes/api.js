ad6a7eab8c173c64aa3510272b2a7d046f7c25f7
margaritasing <libiamfreitesm@gmail.com>
2022-06-18 12:01:49 -0300

    agrego cambios

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const middleware = require('./middleware')
 
 const apiFilmRouter = require('./api/films');
 const apiUserRouter = require('./api/users');
 
-router.use('/films', apiFilmRouter);
+router.use('/films', middleware.checkToken , apiFilmRouter);
 router.use('/users', apiUserRouter);
 
 
