e80278dfe7ab55382206b9eac22c7c49efc190fe
margaritasing <libiamfreitesm@gmail.com>
2022-06-18 16:06:37 -0300

    agrego cambios en el db

--- db.js
+++ db.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 
 const FilmModel = require('./models/films');
 const UserModel = require('./models/user');
+const PeliculaModel = require('./models/pelicula');
+const GeneroModel = require('./models/genero');
+const PersonajeModel = require('./models/personaje');
+const SerieModel = require('./models/serie');
+/* require('./models/asociations') */
 
 const sequelize = new Sequelize('nsY6MVxofg','nsY6MVxofg','Ij4i5iQMax', {
     host: 'remotemysql.com',
@@ -10,6 +15,19 @@ const sequelize = new Sequelize('nsY6MVxofg','nsY6MVxofg','Ij4i5iQMax', {
 
 const Film = FilmModel(sequelize, Sequelize);
 const User = UserModel(sequelize, Sequelize);
+const Personaje = PersonajeModel(sequelize, Sequelize);
+const Serie = SerieModel(sequelize, Sequelize);
+const Pelicula = PeliculaModel(sequelize, Sequelize);
+const Genero = GeneroModel(sequelize, Sequelize)
+
+
+//Creo la relacion de la tabla, personaje y pelicula 
+Pelicula.belongsToMany(Personaje, { through: 'Pelicula_Personajes' })
+Personaje.belongsToMany(Pelicula, { through: 'Pelicula_Personajes' })
+
+
+
+
 
 sequelize.sync({force: false})
 .then(()=>{
@@ -18,6 +36,11 @@ sequelize.sync({force: false})
 
 module.exports ={
     Film,
-    User
+    User, 
+    Personaje,
+    Serie,
+    Genero,
+    Pelicula,
+  
 };
 
