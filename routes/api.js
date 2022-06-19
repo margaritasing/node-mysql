be38ed79dbe9ed16a0d6eca8bca88d68dc1ea9de
margaritasing <libiamfreitesm@gmail.com>
2022-06-19 15:59:02 -0300

    agrego routes

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,13 @@ const middleware = require('./middleware')
 
 const apiFilmRouter = require('./api/films');
 const apiUserRouter = require('./api/users');
+const apiPersonajeRouter = require('./api/personaje');
+const apiPeliculaRouter = require('./api/pelicula');
 
 router.use('/films', middleware.checkToken , apiFilmRouter);
 router.use('/users', apiUserRouter);
+router.use('/personajes',middleware.checkToken,apiPersonajeRouter);
+router.use('/peliculas', middleware.checkToken, apiPeliculaRouter);
 
 
 module.exports = router;
