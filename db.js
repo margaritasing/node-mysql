48c5ae708a4125c8a121d5e75e1cbec29c102aa5
margaritasing <libiamfreitesm@gmail.com>
2022-06-19 15:57:04 -0300

    subo cambios

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const PeliculaModel = require('./models/pelicula');
 const GeneroModel = require('./models/genero');
 const PersonajeModel = require('./models/personaje');
 const SerieModel = require('./models/serie');
-/* require('./models/asociations') */
+
 
 const sequelize = new Sequelize('nsY6MVxofg','nsY6MVxofg','Ij4i5iQMax', {
     host: 'remotemysql.com',
@@ -22,8 +22,20 @@ const Genero = GeneroModel(sequelize, Sequelize)
 
 
 //Creo la relacion de la tabla, personaje y pelicula 
-Pelicula.belongsToMany(Personaje, { through: 'Pelicula_Personajes' })
-Personaje.belongsToMany(Pelicula, { through: 'Pelicula_Personajes' })
+Pelicula.belongsToMany(Personaje, { through: 'Pelicula_Personajes' });
+Personaje.belongsToMany(Pelicula, { through: 'Pelicula_Personajes' });
+
+//Creo la relacion de la tabla de peliculas con la de genero
+Pelicula.belongsToMany(Genero, { through:'Genero_Pelicula' });
+Genero.belongsToMany(Pelicula, { through: 'Genero_Pelicula' });
+
+//Creo la relacion de la tabla, personaje y pelicula 
+Serie.belongsToMany(Personaje, { through: 'Serie_Personajes' });
+Personaje.belongsToMany(Serie, { through: 'Serie_Personajes' });
+
+//Creo la relacion de la tabla de peliculas con la de genero
+Serie.belongsToMany(Genero, { through:'Genero_Serie' });
+Genero.belongsToMany(Serie, { through: 'Genero_Serie' });
 
 
 
@@ -41,6 +53,7 @@ module.exports ={
     Serie,
     Genero,
     Pelicula,
+    
   
 };
 
